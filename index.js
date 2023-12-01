eaf6b64398a0d999759a54c30c84996ca097513f
SumiAkter2 <akteralisha36@gmail.com>
2023-12-01 08:23:51 +0600

    delete user

--- index.js
+++ index.js
@@ -63,6 +63,13 @@ async function run() {
       const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // menu collection apis
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
